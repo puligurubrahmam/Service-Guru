ed9d2d1e7529543d62b348e8e1bd0cc8a72dc615
puligurubrahmam <147025676+puligurubrahmam@users.noreply.github.com>
2025-05-07 13:16:59 +0530

    index.js

--- frontend/src/components/BizLogin/index.js
+++ frontend/src/components/BizLogin/index.js
@@ -49,7 +49,7 @@ class BizLogin extends Component
         return(
             <div className='login-page'>
                 <div className='logo-div'>
-                    <img className="logo" src="https://s3-alpha-sig.figma.com/img/a7c2/c95a/e57df5f03ddceb5a4011eb1efd953170?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=i7lvM1s0i74MfggIel4C7VrexlGfffkxShALGiS99Txtnir6kf9cE0wDtpfwUiHXVw0BRZQHQIfzxZw1EQ0H1ClfCtFjay3rQCAIS~nSYjLOjg2TBlpcxwCRke5MQCppl-mEDFaqK3aVya7rCuiCkN2hntF9bpWkY8IHJUAJHnAT3XawIK1KXfQdPSW8NrdCJX0OPs0WWHm1JoUY~g374aVLigYQeWKFPHWCLbp7LdkPNRoL~08fbV~qj1viBUfox9ktE9IfCzq6zwxjitIwcAaYxJhH7o77FBChfuYVChd6aYDxaStNv9~tWtzOPwKUir3zVend3~SPe9Jc-FP5VA__"/>
+                    <img className="logo" src="https://i.pinimg.com/originals/09/f5/27/09f52772454cb6c8589681f32bbab479.jpg"/>
                 </div>
                 <div className='login-container'>
                     <h1>Workers Login</h1>
